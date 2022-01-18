9c05c1b88e5a7e99addf37562737bf3aa68bc173
Balás Botond Barna <buduboti@yahoo.com>
2022-01-18 14:15:07 +0200

    init

---
+++ node-proxy.js
@@ -0,0 +1,49 @@
+const http = require('http'),
+  url = require('url'),
+  axios = require('axios');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const query = url.parse(req.url, true).query;
+  console.log('REQUEST GOT:');
+  console.log(`${query.method} ${query.url}`);
+  // console.log(req.rawHeaders);
+  // console.log(req);
+
+  const httpOptions = {
+	  proxy: {
+		  host: '172.16.0.1',
+		  port: 3128,
+		  auth: {
+			  username: 'pxuser',
+			  password: 'Q22'
+		  }
+	  }
+  };
+
+  let promise;
+
+  if (query.method == 'GET') {
+    promise = axios.get(query.url, httpOptions);
+  } else {
+    console.log(`${query.method} method isn't supported yet.`);
+    res.end(`${query.method} method isn't supported yet.`);
+    return;
+  }
+
+  promise.then((resp) => {
+    console.log('RESPONSE:');
+    console.log(`${resp.status} ${resp.statusText}`);
+    console.log(resp.headers);
+    console.log(resp.data);
+    // console.log(resp);
+    res.end(resp.data)
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
